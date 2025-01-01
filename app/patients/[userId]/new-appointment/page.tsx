985ef57667e4cee9522e96904745cb7a190f49e2
Amaan <amaanshaikh.gg@gmail.com>
2025-01-01 22:03:08 +0530

    updated

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,39 @@
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className=" remove-scrollbar container my-auto transform scale-[85%]">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-6 h-10 w-fit"
+          />
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+
+          <p className="copyright py-12">© CuraLink | 2024</p>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
