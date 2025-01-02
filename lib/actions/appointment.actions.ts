1d215ceff6e9e7459863c7f99193a486b9d5d9e5
Amaan <amaanshaikh.gg@gmail.com>
2025-01-03 00:04:18 +0530

    updated

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,7 +1,8 @@
 "use server"
-import { ID } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 import { APPOINTMENT_COLLECTION_ID,  DATABASE_ID, databases} from "../appwrite.config"
 import { parseStringify } from "../utils"
+import { Appointment } from "@/types/appwrite.types"
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -17,3 +18,82 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
         console.log(error)
     }
 }
+
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId,
+        )
+
+        return parseStringify(appointment)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getRecentAppointmentList = async () => {
+    try {
+      const appointments = await databases.listDocuments(
+        DATABASE_ID!,
+        APPOINTMENT_COLLECTION_ID!,
+        [Query.orderDesc("$createdAt")]
+      );
+      //   appointments.documents as Appointment[]
+      // ).filter((appointment) => appointment.status === "scheduled");
+  
+      // const pendingAppointments = (
+      //   appointments.documents as Appointment[]
+      // ).filter((appointment) => appointment.status === "pending");
+  
+      // const cancelledAppointments = (
+      //   appointments.documents as Appointment[]
+      // ).filter((appointment) => appointment.status === "cancelled");
+  
+      // const data = {
+      //   totalCount: appointments.total,
+      //   scheduledCount: scheduledAppointments.length,
+      //   pendingCount: pendingAppointments.length,
+      //   cancelledCount: cancelledAppointments.length,
+      //   documents: appointments.documents,
+      // };
+  
+      const initialCounts = {
+        scheduledCount: 0,
+        pendingCount: 0,
+        cancelledCount: 0,
+      };
+  
+      const counts = (appointments.documents as Appointment[]).reduce(
+        (acc, appointment) => {
+          switch (appointment.status) {
+            case "scheduled":
+              acc.scheduledCount++;
+              break;
+            case "pending":
+              acc.pendingCount++;
+              break;
+            case "cancelled":
+              acc.cancelledCount++;
+              break;
+          }
+          return acc;
+        },
+        initialCounts
+      );
+  
+      const data = {
+        totalCount: appointments.total,
+        ...counts,
+        documents: appointments.documents,
+      };
+  
+      return parseStringify(data);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the recent appointments:",
+        error
+      );
+    }
+  };
