ce711341964b940f6457a9a15eae65c6e8636274
Amaan <amaanshaikh.gg@gmail.com>
2025-01-04 16:19:44 +0530

    initial commit

---
+++ components/AppointmentModal.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Appointment } from "@/types/appwrite.types";
+import AppointmentForm from "./forms/AppointmentForm";
+
+const AppointmentModal = ({
+  type,
+  patientId,
+  userId,
+  appointment,
+}: {
+  type: "schedule" | "cancel";
+  patientId: string;
+  userId: string;
+  appointment?: Appointment;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={`capitalize ${type === "schedule" && "text-green-500"}`}
+        >
+          {type}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dailog sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-3">
+          <DialogTitle className="capitalize">{type} Appointment</DialogTitle>
+          <DialogDescription>
+            Please fill in the form below to {type} the appointment.
+          </DialogDescription>
+        </DialogHeader>
+
+        <AppointmentForm
+          userId={userId}
+          patientId={patientId}
+          type={type}
+          appointment={appointment}
+          setIsOpen={setIsOpen}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AppointmentModal;
